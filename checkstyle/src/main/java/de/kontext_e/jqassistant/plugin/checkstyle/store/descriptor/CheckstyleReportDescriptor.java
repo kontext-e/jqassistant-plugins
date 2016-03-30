7a3efee2dc80f695a2ddd8dd5d2813f6277d59f3
Jens Nerche <j.nerche@kontext-e.de>
2016-03-30 11:13:32 +0200

    refactoring replace FQN by import

--- checkstyle/src/main/java/de/kontext_e/jqassistant/plugin/checkstyle/store/descriptor/CheckstyleReportDescriptor.java
+++ checkstyle/src/main/java/de/kontext_e/jqassistant/plugin/checkstyle/store/descriptor/CheckstyleReportDescriptor.java
@@ -4,6 +4,7 @@ package de.kontext_e.jqassistant.plugin.checkstyle.store.descriptor;
 import java.util.Set;
 
 import com.buschmais.jqassistant.core.store.api.model.Descriptor;
+import com.buschmais.jqassistant.plugin.common.api.model.FileDescriptor;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
 
@@ -11,7 +12,7 @@ import com.buschmais.xo.neo4j.api.annotation.Relation;
  * @author jn4, Kontext E GmbH, 11.02.14
  */
 @Label("Checkstyle")
-public interface CheckstyleReportDescriptor extends Descriptor, com.buschmais.jqassistant.plugin.common.api.model.FileDescriptor {
+public interface CheckstyleReportDescriptor extends Descriptor, FileDescriptor {
 
     @Relation("CHECKSTYLE_FILES")
     Set<CheckstyleFileDescriptor> getFiles();
