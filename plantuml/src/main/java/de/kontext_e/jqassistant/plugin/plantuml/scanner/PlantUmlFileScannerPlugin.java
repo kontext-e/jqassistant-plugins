1e372ef40268a9d3579f5200264d17e5f1c78fb6
Jens Nerche <j.nerche@kontext-e.de>
2016-04-11 16:25:04 +0200

    #8 PlantUML Plugin: make suffix configurable via property

--- plantuml/src/main/java/de/kontext_e/jqassistant/plugin/plantuml/scanner/PlantUmlFileScannerPlugin.java
+++ plantuml/src/main/java/de/kontext_e/jqassistant/plugin/plantuml/scanner/PlantUmlFileScannerPlugin.java
@@ -3,6 +3,8 @@ package de.kontext_e.jqassistant.plugin.plantuml.scanner;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,16 +15,29 @@ import com.buschmais.jqassistant.plugin.common.api.scanner.AbstractScannerPlugin
 import com.buschmais.jqassistant.plugin.common.api.scanner.filesystem.FileResource;
 import de.kontext_e.jqassistant.plugin.plantuml.store.descriptor.PlantUmlFileDescriptor;
 
+import static java.util.Arrays.asList;
+
 public class PlantUmlFileScannerPlugin extends AbstractScannerPlugin<FileResource, PlantUmlFileDescriptor> {
     private static final Logger LOGGER = LoggerFactory.getLogger(PlantUmlFileScannerPlugin.class);
+    public static final String JQASSISTANT_PLUGIN_PLANTUML_SUFFIXES = "jqassistant.plugin.plantuml.suffixes";
+
+    private static List<String> suffixes = asList("puml", "adoc");
 
     @Override
     public boolean accepts(final FileResource item, final String path, final Scope scope) throws IOException {
-        boolean accepted = path.endsWith(".puml") || path.endsWith(".adoc");
-        if(accepted) {
-            LOGGER.info("PlantUML accepted path "+path);
+        int beginIndex = path.lastIndexOf(".");
+        if(beginIndex > 0) {
+            final String suffix = path.substring(beginIndex + 1).toLowerCase();
+
+            boolean accepted = suffixes.contains(suffix);
+            if(accepted) {
+                LOGGER.info("PlantUML accepted path "+path);
+            }
+
+            return accepted;
         }
-        return accepted;
+
+        return false;
     }
 
     @Override
@@ -42,5 +57,16 @@ public class PlantUmlFileScannerPlugin extends AbstractScannerPlugin<FileResourc
         return plantUmlFileDescriptor;
     }
 
+    @Override
+    protected void configure() {
+        super.configure();
 
+        if(getProperties().containsKey(JQASSISTANT_PLUGIN_PLANTUML_SUFFIXES)) {
+            suffixes = new ArrayList<>();
+            String serializedSuffixes = (String) getProperties().get(JQASSISTANT_PLUGIN_PLANTUML_SUFFIXES);
+            for (String suffix : serializedSuffixes.split(",")) {
+                suffixes.add(suffix.toLowerCase().trim());
+            }
+        }
+    }
 }
