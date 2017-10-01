124157a49150279755b7cb279e3b7c8c1a054f76
Jens Nerche <j.nerche@kontext-e.de>
2017-10-01 16:47:59 +0200

    upgraded to jQA 1.3

--- findbugs/src/main/java/de/kontext_e/jqassistant/plugin/findbugs/store/descriptor/FindBugsSourceLineContainingDescriptor.java
+++ findbugs/src/main/java/de/kontext_e/jqassistant/plugin/findbugs/store/descriptor/FindBugsSourceLineContainingDescriptor.java
@@ -1,7 +1,11 @@
 package de.kontext_e.jqassistant.plugin.findbugs.store.descriptor;
 
+import com.buschmais.xo.api.annotation.Abstract;
+import com.buschmais.xo.neo4j.api.annotation.Label;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
 
+@Abstract
+@Label("SourceLineContainer")
 public interface FindBugsSourceLineContainingDescriptor extends FindBugsDescriptor {
 
     @Relation("HAS_SOURCELINE")
