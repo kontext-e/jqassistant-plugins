365ff454340ab25370de136cca23462e7f498baa
Jens Nerche <j.nerche@kontext-e.de>
2018-01-06 17:08:50 +0100

    noted an idea on javaparser

---
+++ javaparser/src/main/java/de/kontext_e/jqassistant/plugin/javaparser/scanner/JavaparserScanner.java
@@ -0,0 +1,13 @@
+package de.kontext_e.jqassistant.plugin.javaparser.scanner;
+
+public class JavaparserScanner {
+    /*
+
+        private void foo() {
+        final org.neo4j.graphdb.GraphDatabaseService graphDatabaseService = store.getGraphDatabaseService();
+        final Node compilationUnit = graphDatabaseService.createNode(label("CompilationUnit"));
+        compilationUnit.setProperty("name", "root");
+    }
+
+     */
+}
