fb6c83522653be58642c71ba0baf8bbd7b4afc75
Jens Nerche <j.nerche@kontext-e.de>
2018-06-15 11:59:20 +0200

    removed the Abstract annotation

--- javaparser/src/main/java/de/kontext_e/jqassistant/plugin/javaparser/store/descriptor/JavaSourceDescriptor.java
+++ javaparser/src/main/java/de/kontext_e/jqassistant/plugin/javaparser/store/descriptor/JavaSourceDescriptor.java
@@ -2,10 +2,8 @@ package de.kontext_e.jqassistant.plugin.javaparser.store.descriptor;
 
 
 import com.buschmais.jqassistant.core.store.api.model.Descriptor;
-import com.buschmais.xo.api.annotation.Abstract;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 
 @Label("JavaSource")
-@Abstract
 public interface JavaSourceDescriptor extends Descriptor {
 }
