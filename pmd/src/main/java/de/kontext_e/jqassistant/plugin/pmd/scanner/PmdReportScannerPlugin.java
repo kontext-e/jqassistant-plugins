1c3c1b5c13bcc5c0f2b4cc00a36ffe3531fabb09
Jens Nerche <j.nerche@kontext-e.de>
2018-12-19 10:03:20 +0100

    backported PMD migration to DOM from 1 7 0-SNAPSHOT

--- pmd/src/main/java/de/kontext_e/jqassistant/plugin/pmd/scanner/PmdReportScannerPlugin.java
+++ pmd/src/main/java/de/kontext_e/jqassistant/plugin/pmd/scanner/PmdReportScannerPlugin.java
@@ -2,14 +2,17 @@ package de.kontext_e.jqassistant.plugin.pmd.scanner;
 
 import java.io.IOException;
 import java.io.InputStream;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.transform.stream.StreamSource;
-
+import java.util.function.Consumer;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import com.buschmais.jqassistant.core.scanner.api.Scanner;
 import com.buschmais.jqassistant.core.scanner.api.ScannerPlugin;
@@ -18,11 +21,6 @@ import com.buschmais.jqassistant.core.store.api.Store;
 import com.buschmais.jqassistant.plugin.common.api.model.FileDescriptor;
 import com.buschmais.jqassistant.plugin.common.api.scanner.AbstractScannerPlugin;
 import com.buschmais.jqassistant.plugin.common.api.scanner.filesystem.FileResource;
-
-import de.kontext_e.jqassistant.plugin.pmd.jaxb.FileType;
-import de.kontext_e.jqassistant.plugin.pmd.jaxb.ObjectFactory;
-import de.kontext_e.jqassistant.plugin.pmd.jaxb.PmdType;
-import de.kontext_e.jqassistant.plugin.pmd.jaxb.ViolationType;
 import de.kontext_e.jqassistant.plugin.pmd.store.PmdFileDescriptor;
 import de.kontext_e.jqassistant.plugin.pmd.store.PmdReportDescriptor;
 import de.kontext_e.jqassistant.plugin.pmd.store.PmdViolationDescriptor;
@@ -35,20 +33,11 @@ import de.kontext_e.jqassistant.plugin.pmd.store.PmdViolationDescriptor;
 public class PmdReportScannerPlugin extends AbstractScannerPlugin<FileResource, PmdReportDescriptor> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PmdReportScannerPlugin.class);
-     public static final String JQASSISTANT_PLUGIN_PMD_FILENAME = "jqassistant.plugin.pmd.filename";
-     public static final String JQASSISTANT_PLUGIN_PMD_DIRNAME = "jqassistant.plugin.pmd.dirname";
+    public static final String JQASSISTANT_PLUGIN_PMD_FILENAME = "jqassistant.plugin.pmd.filename";
+    public static final String JQASSISTANT_PLUGIN_PMD_DIRNAME = "jqassistant.plugin.pmd.dirname";
 
-    private JAXBContext jaxbContext;
     private String pmdFileName = "pmd.xml";
     private String pmdDirName = "pmd";
-
-    public PmdReportScannerPlugin() {
-        try {
-            jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
-        } catch (JAXBException e) {
-            throw new IllegalStateException("Cannot create JAXB context.", e);
-        }
-    }
 
     @Override
     protected void configure() {
@@ -79,70 +68,129 @@ public class PmdReportScannerPlugin extends AbstractScannerPlugin<FileResource, 
     }
 
     @Override
-    public PmdReportDescriptor scan(final FileResource file, String path, Scope scope, Scanner scanner) throws IOException {
-        LOGGER.debug("Pmd scans path "+path);
-        final PmdType pmdType = unmarshalPmdXml(file.createStream());
-		FileDescriptor fileDescriptor = scanner.getContext().getCurrentDescriptor();
-		final PmdReportDescriptor pmdReportDescriptor = scanner.getContext().getStore().addDescriptorType(fileDescriptor, PmdReportDescriptor.class);
-		pmdReportDescriptor.setVersion(pmdType.getVersion());
-		if(pmdType.getTimestamp()!=null) {
-			pmdReportDescriptor.setTimestamp(pmdType.getTimestamp().toString());
-		}
-		readFiles(scanner.getContext().getStore(), pmdType, pmdReportDescriptor);
-        return pmdReportDescriptor;
-    }
-
-    private void readFiles(final Store store, final PmdType pmdType, final PmdReportDescriptor pmdReportDescriptor) {
-        for (FileType fileType : pmdType.getFile()) {
-            final PmdFileDescriptor pmdFileDescriptor = store.create(PmdFileDescriptor.class);
-            pmdFileDescriptor.setName(truncateName(fileType.getName()));
-            
-            //create fqn from first Violation element attributes:
-            String classname = fileType.getViolation().get(0).getClazz();
-            String packagename = fileType.getViolation().get(0).getPackage();
-            String fqn = packagename+"."+classname;            
-            pmdFileDescriptor.setFullQualifiedName(fqn);
-            
-            pmdReportDescriptor.getFiles().add(pmdFileDescriptor);
-            readViolations(store, fileType, pmdFileDescriptor);
-        }
-    }
-
-    private void readViolations(final Store store, final FileType fileType, final PmdFileDescriptor pmdFileDescriptor) {
-        for (ViolationType vioType : fileType.getViolation()) {
-            final PmdViolationDescriptor vioDescriptor = store.create(PmdViolationDescriptor.class);
-			vioDescriptor.setBeginLine(vioType.getBeginline());
-			vioDescriptor.setEndLine(vioType.getEndline());
-			vioDescriptor.setBeginColumn(vioType.getBegincolumn());
-			vioDescriptor.setEndColumn(vioType.getEndcolumn());
-            vioDescriptor.setRule(vioType.getRule());
-            vioDescriptor.setRuleSet(vioType.getRuleset());
-            vioDescriptor.setPackage(vioType.get_package());
-            vioDescriptor.setClassName(vioType.getClazz());
-			vioDescriptor.setMethod(vioType.getMethod());
-			vioDescriptor.setVariable(vioType.getVariable());
-			vioDescriptor.setExternalInfoUrl(vioType.getExternalInfoUrl());
-			vioDescriptor.setPriority(vioType.getPriority());
-			vioDescriptor.setMessage(vioType.getValue());
-            pmdFileDescriptor.getViolations().add(vioDescriptor);
-        }
-    }
-
-    protected String truncateName(final String name) {
-        if(name.lastIndexOf(System.getProperty("file.separator")) <= 0) {
-            return name;
-        }
-        return name.substring(name.lastIndexOf(System.getProperty("file.separator")) + 1);
-    }
-
-    protected PmdType unmarshalPmdXml(final InputStream streamSource) throws IOException {
-        final PmdType pmdType;
+    public PmdReportDescriptor scan(final FileResource file, String path, Scope scope, Scanner scanner) {
         try {
-            final Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            pmdType = unmarshaller.unmarshal(new StreamSource(streamSource), PmdType.class).getValue();
-        } catch (JAXBException e) {
-            throw new IOException("Cannot read model descriptor.", e);
+            FileDescriptor fileDescriptor = scanner.getContext().getCurrentDescriptor();
+            final PmdReportDescriptor pmdReportDescriptor = scanner.getContext().getStore().addDescriptorType(fileDescriptor, PmdReportDescriptor.class);
+
+            scanViaDom(file.createStream(), pmdReportDescriptor, scanner.getContext().getStore());
+            return pmdReportDescriptor;
+        } catch (Exception e) {
+            LOGGER.warn("Error while scanning a PMD file: "+e, e);
+            return null;
         }
-        return pmdType;
+    }
+
+    private void scanViaDom(final InputStream is, final PmdReportDescriptor pmdReportDescriptor, final Store store) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(is);
+        Node rootNode = doc.getDocumentElement();
+
+        attributes(rootNode, node -> {
+            if("version".equalsIgnoreCase(node.getNodeName())) {
+                pmdReportDescriptor.setVersion(node.getNodeValue());
+            }
+            if("timestamp".equalsIgnoreCase(node.getNodeName())) {
+                pmdReportDescriptor.setTimestamp(node.getNodeValue());
+            }
+        });
+        visitNode(rootNode, node -> importFileNode(node, store, pmdReportDescriptor));
+    }
+
+    private void attributes(final Node child, Consumer<Node> attributeConumser) {
+        if(child == null) return;
+        if(attributeConumser == null) return;
+
+        NamedNodeMap attributes = child.getAttributes();
+        if(attributes == null) return;
+        for(int a = 0; a < attributes.getLength(); a++) {
+            Node attr = attributes.item(a);
+            if(attr == null) continue;
+            attributeConumser.accept(attr);
+        }
+    }
+
+    private void visitNode(Node node, Consumer<Node> nodeConsumer) {
+        NodeList childNodes = node.getChildNodes();
+        for(int i = 0; i < childNodes.getLength(); i++) {
+            Node child = childNodes.item(i);
+            if("#comment".equalsIgnoreCase(child.getNodeName())) continue;
+            nodeConsumer.accept(child);
+        }
+    }
+
+    private void importFileNode(final Node fileNode, final Store store, final PmdReportDescriptor pmdReportDescriptor) {
+        if(!"file".equalsIgnoreCase(fileNode.getNodeName())) return;
+
+        final PmdFileDescriptor pmdFileDescriptor = store.create(PmdFileDescriptor.class);
+        pmdReportDescriptor.getFiles().add(pmdFileDescriptor);
+        attributes(fileNode, node -> {
+            if("name".equalsIgnoreCase(node.getNodeName())) {
+                pmdFileDescriptor.setName(node.getNodeValue());
+            }
+        });
+        visitNode(fileNode, node -> importViolationNode(node, store, pmdFileDescriptor));
+    }
+
+    private void importViolationNode(final Node fileNode, final Store store, final PmdFileDescriptor pmdFileDescriptor) {
+        if(!"violation".equalsIgnoreCase(fileNode.getNodeName())) return;
+
+        final PmdViolationDescriptor vioDescriptor = store.create(PmdViolationDescriptor.class);
+        pmdFileDescriptor.getViolations().add(vioDescriptor);
+        final String[] x = new String[2];
+        attributes(fileNode, node -> {
+            if("beginline".equalsIgnoreCase(node.getNodeName())) {
+                vioDescriptor.setBeginLine(Integer.valueOf(node.getNodeValue()));
+            }
+            if("endline".equalsIgnoreCase(node.getNodeName())) {
+                vioDescriptor.setEndLine(Integer.valueOf(node.getNodeValue()));
+            }
+            if("begincolumn".equalsIgnoreCase(node.getNodeName())) {
+                vioDescriptor.setBeginColumn(Integer.valueOf(node.getNodeValue()));
+            }
+            if("endcolumn".equalsIgnoreCase(node.getNodeName())) {
+                vioDescriptor.setEndColumn(Integer.valueOf(node.getNodeValue()));
+            }
+            if("rule".equalsIgnoreCase(node.getNodeName())) {
+                vioDescriptor.setRule(node.getNodeValue());
+            }
+            if("ruleset".equalsIgnoreCase(node.getNodeName())) {
+                vioDescriptor.setRuleSet(node.getNodeValue());
+            }
+            if("package".equalsIgnoreCase(node.getNodeName())) {
+                if(x[0] == null) {
+                    x[0] = node.getNodeValue();
+                }
+                vioDescriptor.setPackage(node.getNodeValue());
+            }
+            if("class".equalsIgnoreCase(node.getNodeName())) {
+                if(x[1] == null) {
+                    x[1] = node.getNodeValue();
+                }
+                vioDescriptor.setClassName(node.getNodeValue());
+            }
+            if("externalInfoUrl".equalsIgnoreCase(node.getNodeName())) {
+                vioDescriptor.setExternalInfoUrl(node.getNodeValue());
+            }
+            if("priority".equalsIgnoreCase(node.getNodeName())) {
+                vioDescriptor.setPriority(Integer.valueOf(node.getNodeValue()));
+            }
+            if("variable".equalsIgnoreCase(node.getNodeName())) {
+                vioDescriptor.setVariable(node.getNodeValue());
+            }
+            if("method".equalsIgnoreCase(node.getNodeName())) {
+                vioDescriptor.setMethod(node.getNodeValue());
+            }
+        });
+        pmdFileDescriptor.setFullQualifiedName(x[0]+"."+x[1]);
+
+        visitNode(fileNode, node -> {
+            if("#text".equalsIgnoreCase(node.getNodeName())) {
+                String message = node.getNodeValue();
+                message = message.replaceAll("\\n","").trim();
+                vioDescriptor.setMessage(message);
+            }
+        });
     }
 }
