bc549c36a6ab816b741a7c8d47e7b3d8ad159569
Jens Nerche <j.nerche@kontext-e.de>
2019-06-05 08:30:30 +0200

    Excel file descriptor does not need a separate name, fileName is sufficient

--- excel/src/main/java/de/kontext_e/jqassistant/plugin/excel/store/descriptor/ExcelFileDescriptor.java
+++ excel/src/main/java/de/kontext_e/jqassistant/plugin/excel/store/descriptor/ExcelFileDescriptor.java
@@ -1,12 +1,11 @@
 package de.kontext_e.jqassistant.plugin.excel.store.descriptor;
 
-import java.util.List;
-
 import com.buschmais.jqassistant.plugin.common.api.model.FileDescriptor;
-import com.buschmais.jqassistant.plugin.common.api.model.NamedDescriptor;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
 
-public interface ExcelFileDescriptor extends ExcelDescriptor, NamedDescriptor, FileDescriptor {
+import java.util.List;
+
+public interface ExcelFileDescriptor extends ExcelDescriptor, FileDescriptor {
 
   @Relation("HAS_SHEET")
   List<ExcelSheetDescriptor> getSheets();
